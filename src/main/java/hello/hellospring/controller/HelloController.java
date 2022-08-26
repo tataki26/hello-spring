0e45965eb2268b51fd5b906bdfa424ed3730e0cb
tataki26 <tataki26@gmail.com>
2022-08-26 15:19:39 +0900

    Add Hello class and two methods using ResponseBody annotation
    to exercise API(GET)
    
    - return string / return object

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 // 컨트롤러를 정의하기 위한 annotation
 @Controller
@@ -30,6 +31,43 @@ public class HelloController {
     {
         model.addAttribute("name", name);
         return "hello-template";
+
+    }
+
+    // 문자열 전달하기
+    @GetMapping("hello-string")
+    // HTTP(통신 프로토콜)의 body 부분에 리턴 값을 직접 넣어주는 역할
+    // 템플릿 엔진과 다르게 VIEW를 활용하지 않는다
+    // HTML 태그가 필요 없이 넘겨 받은 인자를 그대로 화면에 띄워준다
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    // 데이터 전달하기
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){ // value
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; // json 포맷(key-value)의 데이터가 넘어감 (default)
+    };
+
+    // static 사용 시, class 안에서 또 다른 class 선언이 가능
+    static class Hello {
+        private String name; // key
+
+        // 자바 빈 표준 - 프로퍼티 접근 방식
+        // 외부에서 데이터에 바로 접근할 수 없도록 보호해주는 기능
+        // getter
+        public String getName(){
+            return name;
+        }
+
+        // setter
+        public void setName(String name){
+            this.name = name;
+        }
 
     }
 
