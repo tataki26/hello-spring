4ef72e592959fcbdc94268844ad1fc992bcde154
tataki26 <tataki26@gmail.com>
2022-08-26 10:30:46 +0900

    Add annotation about running main method with SpringApplication

--- src/main/java/hello/hellospring/HelloSpringApplication.java
+++ src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
+
+		// SpringBootApplication이라는 annotation이 달린 클래스를 인자로 넣으면 springboot app 실행
+		// tomcat 웹 서버가 내장되어 있어 app을 자동으로 화면에 띄워 준다
 		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
